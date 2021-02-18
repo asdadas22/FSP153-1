43ea9138ef997220af74df9e5daeb6bc581b7d2a
Ruben Suarez <suarez_pro@hotmail.com>
2021-02-18 19:03:43 -0300

    mostrar datos
    muestro datos en el index.html

--- individual3-m6/app/views.py
+++ individual3-m6/app/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 
 def inicio(request):
-    
-    return render(request, 'app/index.html')
+    padres_obtenidos = Padres.objects.values()
+    hijos_obtenidos = Hijos.objects.values()
+    santas_obtenidos = Santaclaus.objects.values()
+    proveedor_obtenidos = Proveedor.objects.values()
+    cartas_obtenidas = Carta.objects.all()
+    context = {'padres': padres_obtenidos,
+                'hijos': hijos_obtenidos,
+                'santas': santas_obtenidos,
+                'proveedores': proveedor_obtenidos,
+                'cartas': cartas_obtenidas
+                } 
+    return render(request, 'app/index.html', context)
--- individual3-m6/individual3/settings.py
+++ individual3-m6/individual3/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'individual3m6',
         'USER': 'postgres',
-        'PASSWORD':'Lagox69',
+        'PASSWORD':'123456',
         'HOST':'localhost',
         'PORT':'5432',
     }
